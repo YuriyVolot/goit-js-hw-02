3128d08e002b5d7196e769c1886a2baf26a62f74
YuriyVolot <58395375+YuriyVolot@users.noreply.github.com>
2020-05-13 17:49:05 +0300

    add task 01-04

---
+++ js/task-03.js
@@ -0,0 +1,22 @@
+const findLongestWord = function (string) {
+    const arrString = string.split(' ');
+    let wordLength = 0;
+    let longestWord;
+
+    for (let i = 0; i < arrString.length; i += 1) {
+        wordLength = arrString[1].length;
+        if (arrString[i].length > wordLength) {
+            longestWord = arrString[i];
+            return longestWord;
+        }
+    }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
